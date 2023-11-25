4dc0e9d3dd4ff9f8e1d155f40d3baa5748762c3f
dmitriy-tischenko <tischenko.prg@gmail.com>
2023-11-25 17:23:45 +0300

    web-server by tischenko-dmitriy

--- src/main/java/otus/study/cashmachine/webserver/controllers/AccountController.java
+++ src/main/java/otus/study/cashmachine/webserver/controllers/AccountController.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import otus.study.cashmachine.webserver.dto.AccountDto;
 import otus.study.cashmachine.webserver.service.AccountDtoService;
